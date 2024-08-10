68896fc9600b7257d0fe471060acc2247c37df4d
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2024-08-10 22:50:22 +0900

    add FirebaseAnalyticsObserver

---
+++ lib/page2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page2'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Page2',
+              style: Theme.of(context).textTheme.headlineMedium,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
